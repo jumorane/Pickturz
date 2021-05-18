aaa55346a0d6e761ff06793db466d9386248f588
jumorane <jules.mrn01@gmail.com>
2021-05-18 13:09:07 +0200

    Début application titre/css

---
+++ src/comps/Titre.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const titre = () => {
+  return (
+    <div className="title">
+      <h1>Pickturz</h1>
+      <h2>Vos photos</h2>
+    </div>
+  )
+}
+
+export default titre;
