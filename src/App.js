89dac7682d24345d294d53b69029f073155963d4
jumorane <jules.mrn01@gmail.com>
2021-05-24 18:33:45 +0200

    début boutton share

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Title from './comps/Titre';
 import UploadForm from './comps/UploadForm';
 import ImageGrid from './comps/ImageGrid';
 import Modal from './comps/Modal';
+import {EmailShareButton,FacebookMessengerShareButton,PinterestShareButton,TwitterShareButton } from 'react-share';
+import {EmailIcon, FacebookMessengerIcon,PinterestIcon, TwitterIcon, } from 'react-share';
+
+
 
 function App() {
   const [selectedImg, setSelectedImg] = useState(null);
@@ -10,6 +14,13 @@ function App() {
   return (
 
     <div className="App">
+      <container> <segment>
+      <EmailShareButton url="">
+        <EmailIcon logoFillColor="white" round={true}></EmailIcon>
+      </EmailShareButton> 
+     
+  </segment> </container>
+
       <Title/>
       <UploadForm/>
       <ImageGrid setSelectedImg={setSelectedImg} />
@@ -17,6 +28,11 @@ function App() {
         <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
       )}
     </div>
+
+
+
+
+
   );
 }
 
